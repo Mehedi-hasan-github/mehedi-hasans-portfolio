3a18192aee98a126f025bcbecfb5602a661b689e
Mehedi-hasan-github <mehedy4884@gmail.com>
2022-08-19 08:46:27 +0600

    Login and Register page completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Login.modules.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2 >Hello Login</h2>
+        
+           
+            <div className='login-page'>
+            <h1>Login</h1>
+            <form>
+                <div className='login-box'>
+                    <input type="email" required></input>
+                    <label>UserEmail</label>
+                </div>
+                <div className='login-box'>
+                    <input type="password" required></input>
+                    <label>UserPassword</label>
+                </div>
+                <div className='links'><Link id='forgot-password' to="/#">Forgot password?</Link>
+                <Link id='register' to="/register">Register</Link></div><br/>
+                <button>Login</button>
+            </form>           
         </div>
+     
     );
 };
 
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import './Register.modules.css'
+import { Link } from 'react-router-dom';
 const Register = () => {
     return (
-        <div>
+        <div className='register-page'>
             <h1>Please Register</h1>
+            <div className='register-box'>
+                <input type="FullName" required></input>
+                <label>Full Name</label>
+            </div>
+            <div className='register-box'>
+                <input type="email" required></input>
+                <label>Email</label>
+            </div>
+            <div className='register-box'>
+                <input type="password" required></input>
+                <label>Password</label>
+            </div>
+            <div className='register-box'>
+                <input type="password" required></input>
+                <label>Confirm Password</label>
+            </div>
+            <span ><Link id='login' to="/login">Login</Link></span><br/>
+            <button>Register</button>
         </div>
     );
 };
